84385ddf25ee79359ff2fd6de09b8c967958b203
Rafael Moreira <rafael.castro13@hotmail.com>
2025-07-21 21:08:45 -0300

    Atividades praticas 01 Python - respostas de 1 ao 4

---
+++ pratica-01/calcula_preco_total.py
@@ -0,0 +1,23 @@
+"""
+4- Calculadora de Preço Total
+* Desenvolva um programa que calcula o preço total de uma compra. 
+* Use as seguintes informações:
+
+* Nome do produto: "Cadeira Infantil"
+* Preço unitário: R$ 12.40
+* Quantidade: 3
+
+* O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+"""
+
+produto = "Cadeira Infantil"
+preco_uni = 12.40
+quantidade = 3
+
+preco_total = quantidade * preco_uni
+
+print("Nome do produto:", produto)
+print("Preço unitário: R$", preco_uni)
+print("QUantidade:", quantidade)
+
+print("O preço total é: R$", preco_total)
---
+++ pratica-01/calcula_volume.py
@@ -0,0 +1,16 @@
+"""
+3- Calculadora de Volume
+* Crie um programa que calcula o volume de uma caixa retangular. 
+* Use as seguintes dimensões:
+
+* Comprimento: 12 cm
+* Largura: 14 cm
+* Altura: 20 cm
+"""
+comprimento = 12 
+largura = 14
+altura = 20
+
+calcula_volume = comprimento * largura * altura
+resultado = calcula_volume
+print("O volume calculado é: ", resultado,"cm³")
